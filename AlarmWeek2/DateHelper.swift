af045c4327bef837ce6e89b353d4c2e5a6766bb8
reich452 <reich452@regis.edu>
2017-02-13 14:46:10 -0700

    Step one is complete which includes the story board and their tableview files.

---
+++ AlarmWeek2/DateHelper.swift
@@ -0,0 +1,24 @@
+//
+//  DateHelper.swift
+//  Alarm
+//
+//  Created by James Pacheco on 5/9/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import Foundation
+
+enum DateHelper {
+	
+	static var thisMorningAtMidnight: Date? {
+		let calendar = Calendar.current
+		let now = Date()
+        return calendar.date(bySettingHour: 0, minute: 0, second: 0, of: now)
+	}
+	
+	static var tomorrowMorningAtMidnight: Date? {
+		let calendar = Calendar.current
+		guard let thisMorningAtMidnight = thisMorningAtMidnight else { return nil }
+        return calendar.date(byAdding: .day, value: 1, to: thisMorningAtMidnight)
+	}
+}
